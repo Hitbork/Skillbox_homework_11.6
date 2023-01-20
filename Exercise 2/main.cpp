41b9e8a1eb1bb2d367dd56891f1be44fa40193f4
Hitbork <hitbork@yandex.ru>
2023-01-20 10:13:28 +0300

    Exercise 2 - release

--- Exercise 2/main.cpp
+++ Exercise 2/main.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
 
+// Function to check default chars
+bool is_ok_default_chars(std::string text, int index) {
+    if (!((text[index] >= 'a' && text[index] <= 'z') ||
+          (text[index] >= 'A' && text[index] <= 'Z') ||
+          (text[index] >= '1' && text[index] <= '9') ||
+          (text[index] == '-') ||
+          (text[index] == '.' && (index >= 1 && index <= text.length()-2) &&
+          ((index == text.length() - 2) ||
+          ((index <= text.length() - 3) && (text[index + 1] != '.')))))) {
+        return false;
+    }
+
+    return true;
+}
+
+// Function to check additional chars
+bool is_ok_additional_chars(char c) {
+    if ((c == '!') ||
+        ((c >= '#') && (c <= '\'')) ||
+        ((c >= '*') && (c <= '+')) ||
+        (c == '-') ||
+        (c == '/') ||
+        (c == '=') ||
+        (c == '?') ||
+        (c == '^') ||
+        (c == '_') ||
+        (c == '`') ||
+        ((c >= '{') && (c <= '~'))) {
+        return true;
+    }
+
+    return false;
+}
+
 // Function to check if mailbox correct
 bool is_correct_mail(std::string str) {
     // Condition for amount of "@"
-    if (!((str.find("@") != str.rfind("@")) && (str.find("@") != -1))) {
+    if (!((str.find("@") == str.rfind("@")) && (str.find("@") != -1))) {
         return false;
     }
 
@@ -17,7 +51,21 @@ bool is_correct_mail(std::string str) {
         return false;
     }
 
-    // Condition for chars
+    // Conditions for chars
+    for (int i = 0; i < firstPart.length(); i++) {
+        if (!((is_ok_default_chars(firstPart, i)) ||
+            (is_ok_additional_chars(firstPart[i])))) {
+            return false;
+        }
+    }
+
+    for (int i = 0; i < secondPart.length(); i++) {
+        if (!is_ok_default_chars(secondPart, i)) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 int main() {
@@ -31,7 +79,13 @@ int main() {
     std::cout << "Input mail box:\n";
     std::cin >> str;
 
+    std::cout << "\n";
 
     // Output
+    if (is_correct_mail(str)) {
+        std::cout << "Yes\n";
+    } else {
+        std::cout << "No\n";
+    }
     std::cout << (is_correct_mail(str)) ? "Yes" : "No";
 }
